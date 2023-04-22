137f88645ef8d75d039a0ab93d38b50615a46cce
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-22 15:24:04 -0300

    Exrecício concluído

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("");
+Console.WriteLine("      Um lindo Haikai para você :)");
+
+Console.WriteLine("");
+
+    Console.WriteLine("      Voar sempre, cansa");
+    Console.WriteLine("      por isso ela corre");
+    Console.WriteLine("      em passos de dança");
+
+                                Console.WriteLine("                              Eugénea Tabosa");
